2ca73b2f47603f7ae7a559a7ce4cc1a1cde111d7
vidya <vidyarajamanickam19@gmail.com>
2018-07-12 14:31:39 +0530

    forst

---
+++ provision_azurevm.tf
@@ -0,0 +1,156 @@
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+    subscription_id = "ff2ac935-150c-4854-8cb5-044cf21d224c"
+    client_id       = "68ee6abb-6614-45b8-b1a2-3c9291b4eba9"
+    client_secret   = "748cd0ae-21f4-485e-848d-9e0064f788a0"
+    tenant_id       = "105b2061-b669-4b31-92ac-24d304d195dc"
+}
+
+# Create a resource group if it doesn’t exist
+resource "azurerm_resource_group" "vm" {
+    name     = "${var.resource_group_name}"
+    location = "${var.location}"
+    tags     = "${var.tags}"
+}
+
+resource "random_id" "vm-sa" {
+  keepers = {
+    vm_hostname = "${var.vm_hostname}"
+  }
+
+  byte_length = 6
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "myterraformnetwork" {
+    name                = "vmss-vnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "${var.location}"
+    resource_group_name = "${azurerm_resource_group.vm.name}"
+	tags                = "${var.tags}"
+	
+}
+
+# Create subnet
+resource "azurerm_subnet" "myterraformsubnet" {
+    name                 = "mySubnet"
+    resource_group_name  = "${azurerm_resource_group.vm.name}"
+    virtual_network_name = "${azurerm_virtual_network.myterraformnetwork.name}"
+    address_prefix       = "10.0.1.0/24"
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "vm" {
+    name                         = "${var.vm_hostname}-${count.index}-publicIP"
+    location                     = "${var.location}"
+    resource_group_name          = "${azurerm_resource_group.vm.name}"
+    public_ip_address_allocation = "${var.public_ip_address_allocation}"
+	domain_name_label            = "${element(var.public_ip_dns, count.index)}"
+ 
+    tags                         = "${var.tags}"
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "vm" {
+    name                = "myNetworkSecurityGroup"
+    location            = "${azurerm_resource_group.vm.location}"
+    resource_group_name = "${azurerm_resource_group.vm.name}"
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags                         = "${var.tags}"
+}
+
+# Create network interface
+resource "azurerm_network_interface" "vm" {
+    count                     = "${var.nb_instances}"
+    name                      = "nic-${var.vm_hostname}-${count.index}"
+    location                  = "${azurerm_resource_group.vm.location}"
+    resource_group_name       = "${azurerm_resource_group.vm.name}"
+    network_security_group_id = "${azurerm_network_security_group.vm.id}"
+
+    ip_configuration {
+        name                          = "ipconfig${count.index}"
+        subnet_id                     = "${azurerm_subnet.myterraformsubnet.id}"
+        private_ip_address_allocation = "dynamic"
+        public_ip_address_id          = "${length(azurerm_public_ip.vm.*.id
+    }
+
+    
+}
+
+# Generate random text for a unique storage account name
+resource "random_id" "vm-sa" {
+    keepers = {
+        # Generate a new ID only when a new resource group is defined
+        vm_hostname = "${var.vm_hostname}"
+    }
+
+    byte_length = 6
+}
+
+# Create storage account for boot diagnostics
+resource "azurerm_storage_account" "vm-sa" {
+    name                        = "diag${random_id.vm-sa.hex}"
+    resource_group_name         = "${azurerm_resource_group.vm.name}"
+    location                    = "${var.location}"
+    account_tier                = "${element(split("_", var.boot_diagnostics_sa_type),0)}"
+    account_replication_type    = "${element(split("_", var.boot_diagnostics_sa_type),1)}"
+    tags                     = "${var.tags}"
+}
+
+# Create virtual machine
+resource "azurerm_virtual_machine" "myterraformvm" {
+    name                  = "${var.vm_hostname}${count.index}"
+    location              = "${var.location}"
+    resource_group_name   = "${azurerm_resource_group.vm.name}"
+    network_interface_ids = ["${element(azurerm_network_interface.vm.*.id, count.index)}"]
+    vm_size               = "${var.vm_size}"
+
+    storage_os_disk {
+        name              = "datadisk-${var.vm_hostname}"
+        caching           = "ReadWrite"
+        create_option     = "FromImage"
+        managed_disk_type = "${var.storage_account_type}"
+    }
+
+    storage_image_reference {
+        publisher = "${var.os_publisher}"
+        offer     = "${var.vm_os_offer}"
+        sku       = "${var.vm_os_sku}"
+        version   = "${var.vm_os_version}"
+    }
+
+    os_profile {
+        computer_name  = "${var.vm_hostname}${count.index}"
+        admin_username = "${var.admin_username}"
+    }
+
+    os_profile_linux_config {
+        disable_password_authentication = true
+        ssh_keys {
+            path     = "/home/${var.admin_username}/.ssh/authorized_keys"
+            key_data = "${file("${var.ssh_key}")}"
+        }
+    }
+    tags = "${var.tags}"
+    boot_diagnostics {
+        enabled = "${var.boot_diagnostics}"
+        storage_uri = "${var.boot_diagnostics == "true" ? join(",", azurerm_storage_account.vm-sa.*.primary_blob_endpoint) : "" }"
+    }
+
+    
+        
+	
+}
---
+++ variables.tf
@@ -0,0 +1,92 @@
+variable "resource_group_name" {
+  description = "The name of the resource group in which the resources will be created"
+  name     = "terraformRG"
+}
+variable "location" {
+  description = "The location/region where the virtual network is created. Changing this forces a new resource to be created."
+  default = "eastus"
+}
+variable "tags" {
+  type        = "map"
+  description = "A map of the tags to use on the resources that are deployed with this module."
+
+  default = {
+    source = "terraform"
+  }
+}
+
+variable "vm_hostname" {
+  description = "local name of the VM"
+  default     = "Terraformvm"
+}
+
+variable "public_ip_address_allocation" {
+  description = "Defines how an IP address is assigned. Options are Static or Dynamic."
+  default     = "dynamic"
+}
+variable "public_ip_dns" {
+  description = "Optional globally unique per datacenter region domain name label to apply to each public ip address. e.g. thisvar.varlocation.cloudapp.azure.com where you specify only thisvar here. This is an array of names which will pair up sequentially to the number of public ips defined in var.nb_public_ip. One name or empty string is required for every public ip. If no public ip is desired, then set this to an array with a single empty string."
+  default     = [""]
+}
+variable "nb_instances" {
+  description = "Specify the number of vm instances"
+  default     = "1"
+}
+
+variable "vm_size" {
+  description = "Specifies the size of the virtual machine."
+  default     = "Standard_DS1_V2"
+}
+variable "data_disk_size_gb" {
+  description = "Storage data disk size size"
+  default     = "2GB"
+}
+variable "storage_account_type" {
+  description = "Defines the type of storage account to be created. Valid options are Standard_LRS, Standard_ZRS, Standard_GRS, Standard_RAGRS, Premium_LRS."
+  default     = "Premium_LRS"
+}
+variable "os_publisher" {
+  description = "Defines the publisher"
+  default     = "Canonical"
+}
+variable "vm_os_offer" {
+  description = "The name of the offer of the image that you want to deploy. This is ignored when vm_os_id or vm_os_simple are provided."
+  default     = "UbuntuServer"
+}
+
+variable "vm_os_sku" {
+  description = "The sku of the image that you want to deploy. This is ignored when vm_os_id or vm_os_simple are provided."
+  default     = "16.04.0-LTS"
+}
+
+variable "vm_os_version" {
+  description = "The version of the image that you want to deploy. This is ignored when vm_os_id or vm_os_simple are provided."
+  default     = "latest"
+}
+variable "admin_username" {
+  description = "The admin username of the VM that will be deployed"
+  default     = "dxccnaadmin"
+}
+variable "ssh_key" {
+  description = "Path to the public key to be used for ssh access to the VM.  Only used with non-Windows vms and can be left as-is even if using Windows vms. If specifying a path to a certification on a Windows machine to provision a linux vm use the / in the path versus backslash. e.g. c:/home/id_rsa.pub"
+  default     = "~/.ssh/id_rsa.pub"
+}
+variable "boot_diagnostics" {
+  description = "(Optional) Enable or Disable boot diagnostics"
+  default     = "false"
+}
+
+variable "nb_instances" {
+  description = "Specify the number of vm instances"
+  default     = "1"
+}
+variable "boot_diagnostics_sa_type" {
+  description = "(Optional) Storage account type for boot diagnostics"
+  default     = "Standard_LRS"
+}
+
+
+
+
+
+
