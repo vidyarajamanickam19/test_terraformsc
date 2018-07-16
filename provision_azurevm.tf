1e2bf8c8f0bd8267ee093238e4fc69f85268144b
vidyarajamanickam19 <39183353+vidyarajamanickam19@users.noreply.github.com>
2018-07-16 10:20:37 +0530

    Update provision_azurevm.tf

--- provision_azurevm.tf
+++ provision_azurevm.tf
@@ -70,6 +70,17 @@ resource "azurerm_network_security_group" "vm" {
         source_address_prefix      = "*"
         destination_address_prefix = "*"
     }
+	 security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
 
     tags                         = "${var.tags}"
 }
